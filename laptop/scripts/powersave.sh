f83c9c3196400e27f561cfdf5c70a6c2d207213a
Ethan Frei <ethan.frei@gmail.com>
2015-01-09 13:45:02 -0600

    added scripts and powersaving hotkeys

---
+++ laptop/scripts/powersave.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case "$1" in
+    high)
+        xbacklight -set 80 && xset s 300 300 +dpms && xautolock -exit; sleep 1 && xautolock -time 20 -locker "~/scripts/i3exit.sh suspend" &
+        ;;
+    low)
+        xbacklight -set 10 && xset s 60 60 +dpms && xautolock -exit; sleep 1 && xautolock -time 3 -locker "~/scripts/i3exit.sh suspend" &
+        ;;
+    music)
+        xset s 10 10 +dpms && xautolock -disable;
+        ;;
+    none)
+        xset -dpms && xset s off && xautolock -disable;
+        ;;
+    *)
+        echo "Usage: $0 {high|low|music|none}"
+        exit 2
+esac
+
+exit 0
