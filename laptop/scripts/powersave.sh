2d3325b56fb31c4584c797318e1be4e2b63960db
Ethan Frei <ethan.frei@gmail.com>
2016-11-01 18:47:30 -0500

    random changes

--- laptop/scripts/powersave.sh
+++ laptop/scripts/powersave.sh
@@ -5,7 +5,7 @@ case "$1" in
         xbacklight -set 80 && xset s 300 300 +dpms && xautolock -exit; sleep 1 && xautolock -time 20 -locker "~/scripts/i3exit.sh suspend" &
         ;;
     low)
-        xbacklight -set 10 && xset s 60 60 +dpms && xautolock -exit; sleep 1 && xautolock -time 3 -locker "~/scripts/i3exit.sh suspend" &
+        xbacklight -set 4 && xset s 60 60 +dpms && xautolock -exit; sleep 1 && xautolock -time 3 -locker "~/scripts/i3exit.sh suspend" &
         ;;
     music)
         xset s 10 10 +dpms && xautolock -disable;
